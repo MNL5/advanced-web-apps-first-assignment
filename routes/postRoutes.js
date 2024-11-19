16641e1aa9088983b16b83ecb5d7ebf74ce102b2
Matan-Leibovich <matan6142@gmail.com>
2024-11-19 16:06:49 +0200

    Add FindPostById Endpoint

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
-import { getAllPosts, createPost } from "../controllers/postsController.js";
+import {
+  getAllPosts,
+  createPost,
+  getPostById,
+} from "../controllers/postsController.js";
 
 const router = Router();
 router.get("/", getAllPosts);
 
 router.post("/", createPost);
+
+router.get("/:id", getPostById);
 
 export default router;
