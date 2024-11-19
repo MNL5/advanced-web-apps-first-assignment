644518263173cca0ae8dafcb32771743fe5870cd
Matan-Leibovich <matan6142@gmail.com>
2024-11-19 13:20:00 +0200

    Change require to import

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const express = require("express");
+import express from "express";
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT;
 
-const mongoose = require("mongoose");
-mongoose.connect(process.env.DB_CONNECT);
-const db = mongoose.connection;
+import { connect, connection } from "mongoose";
+connect(process.env.DB_CONNECT);
+const db = connection;
 db.on("error", (error) => console.error("DB Error:" + error));
 db.once("open", () => console.log("Connected to database"));
 
-const bodyParser = require("body-parser");
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+import { json, urlencoded } from "body-parser";
+app.use(json());
+app.use(urlencoded({ extended: true }));
 
-const postsRoute = require("./routes/postRoutes");
+import postsRoute from "./routes/postRoutes";
 app.use("/posts", postsRoute);
 
 app.listen(port, () => {
